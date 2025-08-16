bbd4ca1b47af5fcfdcfe39aa14da81133715a214
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2025-08-16 12:34:32 +0900

    Time: 0 ms (100%), Space: 54.2 MB (87.61%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.ts
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+    const res = [];
+
+    function inorder(node: TreeNode | null) {
+        if (!node) return;
+        inorder(node.left);
+        res.push(node.val);
+        inorder(node.right);
+    }
+
+    inorder(root);
+    return res;
+};
