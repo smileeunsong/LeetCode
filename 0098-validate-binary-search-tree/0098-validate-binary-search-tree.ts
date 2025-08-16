fe754de9dba1315e35c75a965c71220195dba188
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2025-08-16 12:48:38 +0900

    Time: 3 ms (16.96%), Space: 60.1 MB (40.96%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isValidBST(root: TreeNode | null): boolean {
+    const inorderRes = [];
+
+    function inorder(node: TreeNode | null) {
+        if (!node) return;
+        inorder(node.left);
+        inorderRes.push(node.val);
+        inorder(node.right);
+    }
+
+    inorder(root);
+    for (let i = 0; i <= inorderRes.length; i++) {
+        if (inorderRes[i] >= inorderRes[i + 1]) return false;
+    }
+
+    return true;
+};
