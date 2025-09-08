0206238311535aa9e500134697179333bfe7422e
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2025-09-08 20:10:04 +0900

    Time: 0 ms (100%), Space: 58 MB (67.69%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.ts
+++ 0101-symmetric-tree/0101-symmetric-tree.ts
@@ -11,8 +11,20 @@
  *     }
  * }
  */
-
 function isSymmetric(root: TreeNode | null): boolean {
+    if (!root) return true;
+    return isMirror(root.left, root.right);
+}
+
+function isMirror(left: TreeNode | null, right: TreeNode | null): boolean {
+    if (!left && !right) return true;
+    if (!left || !right) return false;
+    if (left.val !== right.val) return false;
+
+    return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+}
+
+function isSymmetricStack(root: TreeNode | null): boolean {
     if (!root) return true;
 
     const stack: [TreeNode | null, TreeNode | null][] = [[root.left, root.right]]
