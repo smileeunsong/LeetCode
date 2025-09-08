4b0b4dca4f472c5a2cfde198bf945b7d851f0b6c
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2025-09-08 20:09:24 +0900

    Time: 0 ms (100%), Space: 58.6 MB (22.9%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSymmetric(root: TreeNode | null): boolean {
+    if (!root) return true;
+
+    const stack: [TreeNode | null, TreeNode | null][] = [[root.left, root.right]]
+    while (stack.length > 0) {
+        const [left, right] = stack.pop();
+
+        if (!left && !right) continue;
+        if (!left || !right) return false;
+        if (left.val !== right.val) return false;
+
+        stack.push([left.left, right.right]);
+        stack.push([left.right, right.left]);
+    }
+
+    return true;
+};
