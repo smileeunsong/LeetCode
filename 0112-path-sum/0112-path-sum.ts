caa063fa0c1e59f55a1d55db2908ce91f6ef8d06
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2025-09-14 23:13:03 +0900

    Time: 3 ms (12.05%), Space: 60.6 MB (10.8%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+    if (!root) return false;
+
+    const stack: [TreeNode, number][] = [[root, root.val]]
+    while (stack.length > 0) {
+        const [node, sum] = stack.pop();
+
+        if (node.left) stack.push([node.left, sum + node.left.val])
+        if (node.right) stack.push([node.right, sum + node.right.val])
+        if (!node.left && !node.right && sum === targetSum) return true;
+    }
+
+    return false;
+};
