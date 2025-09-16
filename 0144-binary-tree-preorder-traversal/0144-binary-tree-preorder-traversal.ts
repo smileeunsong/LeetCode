f21aececa3b0db83d93aec97affbd697c8c598fc
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2025-09-16 21:38:47 +0900

    Time: 0 ms (100%), Space: 55.4 MB (51.99%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function preorderTraversal(root: TreeNode | null): number[] {
+    if (!root) return [];
+    
+    const stack: TreeNode[] = [root];
+    const result: number[] = [];
+
+    while (stack.length > 0) {
+        const node = stack.pop();
+        result.push(node.val);
+        if (node.right) stack.push(node.right);
+        if (node.left) stack.push(node.left);
+    }
+
+    return result;
+};
