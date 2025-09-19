a42fc3f6db9b725f0dcbd1ba2f1a8e86294689f4
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2025-09-19 22:50:51 +0900

    Time: 0 ms (100%), Space: 57.6 MB (34.02%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function binaryTreePaths(root: TreeNode | null): string[] {
+    if (!root) return [];
+
+    const result = [];
+    const stack: [TreeNode, string][] = [[root, root.val.toString()]];
+    while (stack.length > 0) {
+        const [node, path] = stack.pop();
+
+        if (node.left) stack.push([node.left, path.concat('->', node.left.val.toString())]);
+        if (node.right) stack.push([node.right, path.concat('->', node.right.val.toString())]);
+        if (!node.left && !node.right) result.push(path);
+    }
+
+    return result;
+};
