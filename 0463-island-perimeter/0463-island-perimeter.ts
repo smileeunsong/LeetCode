cfcbcae3a20fd16fd29a970100ab12be01f90651
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2025-09-22 20:48:30 +0900

    Time: 9 ms (58.12%), Space: 65.4 MB (61.78%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.ts
+++ 0463-island-perimeter/0463-island-perimeter.ts
@@ -1,4 +1,24 @@
 function islandPerimeter(grid: number[][]): number {
+    let perimeter = 0;
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] !== 1) continue;
+
+            let adjacent = 0;
+            if (i > 0 && grid[i-1][j] === 1) adjacent++;
+            if (i < grid.length - 1 && grid[i+1][j]) adjacent++;
+            if (j > 0 && grid[i][j-1] === 1) adjacent++;
+            if (j < grid[i].length - 1 && grid[i][j+1] === 1) adjacent++;
+
+            perimeter += 4 - adjacent;
+        }
+    }
+
+    return perimeter;
+}
+
+function islandPerimeter1(grid: number[][]): number {
     let sum = 0;
 
     for (let i = 0; i < grid.length; i++) {
