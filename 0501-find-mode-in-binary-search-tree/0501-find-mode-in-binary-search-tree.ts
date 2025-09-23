073bd26f6ca933a89a41182c9d8910b642d1d539
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2025-09-23 20:09:25 +0900

    Time: 0 ms (100%), Space: 61.5 MB (94.44%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.ts
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.ts
@@ -11,8 +11,39 @@
  *     }
  * }
  */
-
 function findMode(root: TreeNode | null): number[] {
+    let curVal = 0;
+    let curCount = 0;
+    let maxCount = 0;
+    let modes: number[] = [];
+
+    function handleValue(val: number) {
+        if (val !== curVal) {
+            curVal = val;
+            curCount = 0;
+        }
+        curCount++;
+
+        if (curCount > maxCount) {
+            maxCount = curCount;
+            modes = [curVal];
+        } else if (curCount === maxCount) {
+            modes.push(curVal);
+        }
+    }
+
+    function inorder(node: TreeNode | null) {
+        if (!node) return
+        inorder(node.left);
+        handleValue(node.val);
+        inorder(node.right);
+    }
+
+    inorder(root);
+    return modes;
+}
+
+function findModeFirstTry(root: TreeNode | null): number[] {
     const obj: Record<number, number> = {};
     const stack: TreeNode[] = [root];
 
