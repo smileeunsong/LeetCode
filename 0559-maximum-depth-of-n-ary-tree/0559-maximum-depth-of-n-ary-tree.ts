e692800b534400bf0423b9608d9c5a681ba19b2d
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2025-09-30 20:02:22 +0900

    Time: 66 ms (19.8%), Space: 61.4 MB (17.82%) - LeetHub

---
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for _Node.
+ * class _Node {
+ *     val: number
+ *     children: _Node[]
+ * 
+ *     constructor(val?: number, children?: _Node[]) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.children = (children===undefined ? [] : children)
+ *     }
+ * }
+ */
+
+
+function maxDepth(root: _Node | null): number {
+    if (!root) return 0;
+
+    const stack: [_Node, number][] = [[root, 1]];
+    let max: number = 1;
+
+    while (stack.length > 0) {
+        const [node, depth] = stack.pop();
+        
+        if (!node.children.length) {
+            max = Math.max(max, depth);
+        }
+        
+        for (let i = 0; i < node.children.length; i++) {
+            stack.push([node.children[i], depth + 1]);
+        }
+    }
+
+    return max;
+};
