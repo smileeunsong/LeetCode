954d61cbc938868efd8280e15a8d2763833e984a
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2025-10-02 20:00:22 +0900

    Time: 1 ms (75.38%), Space: 60.6 MB (73.85%) - LeetHub

--- 0563-binary-tree-tilt/0563-binary-tree-tilt.ts
+++ 0563-binary-tree-tilt/0563-binary-tree-tilt.ts
@@ -11,35 +11,56 @@
  *     }
  * }
  */
-function sum(node: TreeNode | null): number {
+
+// function sum(node: TreeNode | null): number {
+//     if (!node) return 0;
+//     if (!node.left && !node.right) return node.val;
+
+//     return sum(node.left) + sum(node.right) + node.val;
+// }
+
+// function makeTilt(node: TreeNode) {
+//     const leftChildSum = sum(node.left);
+//     const rightChildSum = sum(node.right);
+//     const tilt = Math.abs(leftChildSum - rightChildSum);
+
+//     return tilt;
+// }
+
+// function findTilt(root: TreeNode | null): number {
+//     if (!root) return 0;
+//     let res: number = 0;
+
+//     const stack: TreeNode[] = [root];
+//     while (stack.length > 0) {
+//         const node = stack.pop();
+
+//         if (!node.left && !node.right) continue;
+//         if (node.left) stack.push(node.left);
+//         if (node.right) stack.push(node.right);
+
+//         res += makeTilt(node)
+//     }
+
+//     return res;
+// };
+
+// 개선된 풀이
+let totalTilt: number = 0;
+
+function dfs(node: TreeNode | null): number {
     if (!node) return 0;
-    if (!node.left && !node.right) return node.val;
 
-    return sum(node.left) + sum(node.right) + node.val;
-}
+    const leftSum = dfs(node.left);
+    const rightSum = dfs(node.right);
+    totalTilt += Math.abs(leftSum - rightSum);
 
-function makeTilt(node: TreeNode) {
-    const leftChildSum = sum(node.left);
-    const rightChildSum = sum(node.right);
-    const tilt = Math.abs(leftChildSum - rightChildSum);
-
-    return tilt;
+    return leftSum + rightSum + node.val;
 }
 
 function findTilt(root: TreeNode | null): number {
-    if (!root) return 0;
-    let res: number = 0;
+    totalTilt = 0;
+    dfs(root);
 
-    const stack: TreeNode[] = [root];
-    while (stack.length > 0) {
-        const node = stack.pop();
-
-        if (!node.left && !node.right) continue;
-        if (node.left) stack.push(node.left);
-        if (node.right) stack.push(node.right);
-
-        res += makeTilt(node)
-    }
-
-    return res;
-};
+    return totalTilt;
+}
