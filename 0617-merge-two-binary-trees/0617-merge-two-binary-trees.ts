2ae8807b622db1dc6a4b99d82c2a42516d7e2144
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2025-10-09 10:08:25 +0900

    Time: 1 ms (76.53%), Space: 64.4 MB (58.16%) - LeetHub

---
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function mergeTrees(root1: TreeNode | null, root2: TreeNode | null): TreeNode | null {
+    if (!root1 && !root2) return null;
+    if (!root1) return root2;
+    if (!root2) return root1;
+
+    const res = new TreeNode(root1.val + root2.val);
+    res.left = mergeTrees(root1.left, root2.left);
+    res.right = mergeTrees(root1.right, root2.right);
+
+    return res;
+};
