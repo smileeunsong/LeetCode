d14f2fcfdaa7166a29a5c61bfb9de8162d9cd8da
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2025-10-22 22:54:55 +0900

    Time: 325 ms (36.99%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,15 +1,10 @@
 -- Write your PostgreSQL query statement below
--- SELECT count(1)
--- FROM Employee e1
--- WHERE managerId IS NOT NULL
--- GROUP BY managerId
-
-SELECT a.name
-FROM (
-    SELECT e1.name, count(e1.id)
-    FROM Employee e1
-    JOIN Employee e2
-    ON e1.id = e2.managerId
-    GROUP BY e1.id, e1.name
-) a
-WHERE a.count >= 5
+SELECT name
+FROM Employee
+WHERE id IN (
+    SELECT managerId
+    FROM Employee
+    WHERE managerId IS NOT NULL
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+)
