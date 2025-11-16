62049afec0da7363a9af559d64d0b15c816859bb
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2025-11-16 18:37:10 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.ts
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.ts
@@ -12,17 +12,29 @@
  * }
  */
 
+// function sumRootToLeaf(root: TreeNode | null): number {
+//     let sum = 0;
+//     const stack: [TreeNode, string][] = [[root, String(root.val)]];
+
+//     while (stack.length > 0) {
+//         const [node, binarySum] = stack.pop();
+
+//         if (node.left) stack.push([node.left, binarySum + node.left.val]);
+//         if (node.right) stack.push([node.right, binarySum + node.right.val]);
+//         if (!node.left && !node.right) sum += Number.parseInt(binarySum, 2);
+//     }
+
+//     return sum;
+// };
+
 function sumRootToLeaf(root: TreeNode | null): number {
-    let sum = 0;
-    const stack: [TreeNode, string][] = [[root, String(root.val)]];
+    const dfs = (node: TreeNode | null, current: number): number =? {
+        if (!node) return 0;
 
-    while (stack.length > 0) {
-        const [node, binarySum] = stack.pop();
+        const nextVal = (current << 1) | node.val;
+        if (!node.left && !node.right) return nextVal;
 
-        if (node.left) stack.push([node.left, binarySum + node.left.val]);
-        if (node.right) stack.push([node.right, binarySum + node.right.val]);
-        if (!node.left && !node.right) sum += Number.parseInt(binarySum, 2);
-    }
-
-    return sum;
-};
+        return dfs(node.left, nextVal) + dfs(node.right, nextVal);
+    };
+    return dfs(root, 0);
+}
