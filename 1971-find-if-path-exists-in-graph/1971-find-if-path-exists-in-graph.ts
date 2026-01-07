adb064b363e52a0cb5872aac9e9e434141ecf167
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2026-01-07 22:55:45 +0900

    Time: 461 ms (87.93%), Space: 159.9 MB (78.74%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.ts
@@ -0,0 +1,26 @@
+function validPath(n: number, edges: number[][], source: number, destination: number): boolean {
+    const adj: number[][] = Array.from({ length: n }, () => [])
+
+    for (const [u, v] of edges) {
+        adj[u].push(v);
+        adj[v].push(u);
+    }
+
+    const visited: boolean[] = Array(n).fill(false);
+    const stack: number[] = [source];
+
+    while (stack.length > 0) {
+        const cur = stack.pop();
+        // visited[cur] = true;
+        if (cur === destination) return true;
+
+        for (const next of adj[cur]) {
+            if (!visited[next]) {
+                visited[next] = true;
+                stack.push(next);
+            }
+        }
+    }
+
+    return false;
+};
