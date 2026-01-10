c18324f1ace4ab9db47749288f6e1343eac6fda9
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2026-01-10 12:28:36 +0900

    Time: 2 ms (16.67%), Space: 61.9 MB (85.42%) - LeetHub

---
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.ts
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function evaluateTree(root: TreeNode | null): boolean {
+    if (!root.left && !root.right) return !!root.val;
+
+    const left = evaluateTree(root.left);
+    const right = evaluateTree(root.right);
+
+    if (root.val === 2) return left || right;
+    if (root.val === 3) return left && right;
+};
