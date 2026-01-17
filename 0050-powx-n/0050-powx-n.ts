06fcda796c372085ca84aa6407e959dc58b4501c
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2026-01-17 23:50:22 +0900

    Time: 0 ms (100%), Space: 54.6 MB (82.16%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.ts
@@ -0,0 +1,10 @@
+function myPow(x: number, n: number): number {
+    if (n < 0) {
+        x = 1/x;
+        n = -n;
+    }
+    if (n === 0) return 1;
+    
+    const half = myPow(x, Math.floor(n / 2));
+    return n % 2 === 0 ? half * half : half * half * x;
+};
