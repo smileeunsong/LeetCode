62c9a590603218ea99b5f88a3a52b51fe7997cd0
Eunsong-Park <111448985+smileeunsong@users.noreply.github.com>
2026-01-20 22:57:07 +0900

    Time: 0 ms (100%), Space: 57.8 MB (54.82%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.ts
+++ 0231-power-of-two/0231-power-of-two.ts
@@ -1,5 +1,9 @@
 function isPowerOfTwo(n: number): boolean {
     if (n <= 0) return false;
-    if (n === 1) return true;
-    return n % 2 === 0 ? isPowerOfTwo(n/2) : false;
+
+    while (n % 2 === 0) {
+        n /= 2;
+    }
+
+    return n === 1;
 };
